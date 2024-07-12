c7bd84cac11cf0b4820428e3254feee1f40e2396
Florian Schaefer <168949185+RyTiXz@users.noreply.github.com>
2024-07-12 15:18:29 +0200

    Finish project

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 // variables for all querrySelectors //
 const container = document.querySelector(".container");
-const startGrit = document.querySelector(".startGrit");
+const startGrid = document.querySelector(".startGrid");
+const changeGridSize = document.querySelector(".changeGridSize");
+const resetGrid = document.querySelector(".reset");
 
 // variables to createElement // 
 const createDiv = document.createElement("div");
+const computedStyle = getComputedStyle(container);
 
-// variables to change style //
-const containerStyle = {
-    display: "flex",
-    flexWrap: "wrap",
-    width: "512px",
-    height: "512px"
-}
+// Array for all the colors
+const colors = [];
 
-const divStyle = {
-    border: "0.5px solid black",
-    padding: "15.5px",
+for (let i = 0; i < 100; i++) {
+  const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  colors.push(randomColor);
 }
 
 // function to create empty divs //
@@ -23,42 +21,48 @@ for (let i = 0; i < (16*16); i++) {
     const createDiv = document.createElement("div");
     container.appendChild(createDiv);
     createDiv.className = "square";
-    Object.assign(createDiv.style, divStyle);
-    Object.assign(container.style, containerStyle);
-    // colors div
     createDiv.addEventListener("mouseover", () => {
-        createDiv.style.backgroundColor = "red";
+        createDiv.style.backgroundColor = colors[(Math.floor(Math.random() * colors.length))];
     });
-    // uncolors div
-    // createDiv.addEventListener("mouseout", () => {
-    //     createDiv.style.backgroundColor = "white";
-    // })
 }
 
+
 // function to create new grid //
+let promptValueString = 0;
+
 function newGrid(prompt) {
+    const newDivSize = 100 - (computedStyle.width.slice(0, -2)/promptValueString)/computedStyle.width.slice(0, -2)*100;
     for (let i = 0; i < (prompt*prompt); i++) {
         const createDiv = document.createElement("div");
         container.appendChild(createDiv);
         createDiv.className = "square";
-        Object.assign(createDiv.style, divStyle);
-        Object.assign(container.style, containerStyle);
-        // colors div
+        createDiv.style.flex = "0 0 calc(var(--containerWidth) - " + newDivSize + "%";
         createDiv.addEventListener("mouseover", () => {
-            createDiv.style.backgroundColor = "red";
+            createDiv.style.backgroundColor = colors[(Math.floor(Math.random() * colors.length))];
         });
-        // uncolors div
-        // createDiv.addEventListener("mouseout", () => {
-        //     createDiv.style.backgroundColor = "white";
-        // })
     }
 }
 
 // eventListener to get user input for grid size, delets old grid, builds up new grid //
-startGrit.addEventListener("click", () => {
-    let promptValue = prompt("Please choose the size of the new grid");
+startGrid.addEventListener("click", () => {
+    do{
+        var promptValue = parseInt(window.prompt("Please enter a number from 1 to 100", ""), 10);
+    }while(isNaN(promptValue) || promptValue > 100 || promptValue < 1);
+    promptValueString = promptValue.toString();
+
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
-    newGrid(promptValue);
+
+    newGrid(promptValueString);
 });
+
+// eventListener to change grid size, without changing the amount of grids
+changeGridSize.addEventListener("click", () => {
+    const oldGridSize = getComputedStyle(container).getPropertyValue('--containerWidth').slice(0, -2);
+    console.log(oldGridSize);
+    do{
+        var promptGridSize = parseInt(window.prompt("Please enter a number from 100 to 1000", ""), 10);
+    }while(isNaN(promptGridSize) || promptGridSize > 1000 || promptGridSize < 100);
+    container.style.setProperty('--containerWidth', promptGridSize+"px");
+})
